9cf18be4203b127a5f5e182649c8ada015359bd8
austin-wang <austin-wang@nec.com.tw>
2017-10-10 09:20:44 +0800

    can't resolve the problem.

--- src/index.js
+++ src/index.js
@@ -1,22 +1,18 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.png';
-import Data from './data.xml';
+import printMe from './print.js';
 
 function component() {
     var element = document.createElement('div');
+    var btn = document.createElement('button');
 
     // Lodash, currnetly included via a script, is required for this line to work
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
 
-    // add the image to our existing div.
-    var myIcon = new Image();
-    myIcon.src = Icon;
+    // add a btn at "Output Management"
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
 
-    element.appendChild(myIcon);
-
-    console.log(Data);
+    element.appendChild(btn);
 
     return element;
 }
