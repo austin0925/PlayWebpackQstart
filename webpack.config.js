4a81c2d1cb718e8b34ce90bf4843e0395e7ffffb
austin-wang <austin-wang@nec.com.tw>
2017-10-10 20:53:19 +0800

    hot-module-replacement step 1

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,24 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
     entry: {
-        app: './src/index.js',
-        print: './src/print.js'
+        app: './src/index.js'
     },
     devtool: 'inline-source-map',
     devServer: {
         contentBase: path.join(__dirname, "./dist"),
-        compress: true,
+        hot: true,
         port: 9999
     },
     plugins:[
         new CleanWebpackPlugin(['dist']),
         new HtmlWebpackPlugin({
             title: 'Output Management'
-        })
+        }),
+        new webpack.HotModuleReplacementPlugin()
     ],
     output: {
         filename: '[name].bundle.js',
