a7c69ef6cc2e910f9f0f2b9206e1946dfda54c5b
austin-wang <austin-wang@nec.com.tw>
2018-04-14 16:35:40 +0800

    1. save the first step.

--- src/index.js
+++ src/index.js
@@ -1,36 +1,12 @@
 import _ from 'lodash';
-import printMe from './print.js';
-import './styles.css';
 
 function component() {
     var element = document.createElement('div');
-    var btn = document.createElement('button');
 
-    // Lodash, currnetly included via a script, is required for this line to work
+    // Lodash, now imported by thsi script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-    // add a btn at "Output Management"
-    btn.innerHTML = 'Click me and check the console!';
-    btn.onclick = printMe;
-
-    element.appendChild(btn);
 
     return element;
 }
 
-// document.body.appendChild(component());
-
-let element = component(); // Store the element to re-render on print.js chagne
-document.body.appendChild(element);
-
-
-if (module.hot){
-    module.hot.accept('./print.js', function(){
-        console.log('Accepting the updated printMe module!');
-        document.body.removeChild(element);
-        document.body.classList.add('background:red');
-        element = component();
-        document.body.appendChild(element);
-        document.body.classList.add('background:grey');
-    })
-}
+document.body.appendChild(component());
